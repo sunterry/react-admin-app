a2ca91ec43f6a24bb59cc484bba4acfc33ea80dd
段通 <15253130515@163.com>
2019-02-16 16:29:07 +0800

    权限配置修改

--- src/layout/container/container.js
+++ src/layout/container/container.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import RouterContainer from '@/layout/routerContainer'
 import { authorizedRoutes, normalRoutes } from '@/router/routes';
 import BasicLayout from '@/layout/basicLayout';
 import NormalLayout from '@/layout/normalLayout';
 import NotFound from '@/layout/notFound';
 
-export default () => {
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+};
+
+const Container = (props) => {
+  const { user } = props;
 	return (
 		<RouterContainer
+      authorities={ user.auth }
 			authorizedRoutes={ authorizedRoutes }
 			normalRoutes={ normalRoutes }
 			authorizedLayout={ BasicLayout }
@@ -15,4 +25,10 @@ export default () => {
 			notFound={ NotFound }
 		/>
 	)
-}
+};
+
+Container.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps)(Container);
